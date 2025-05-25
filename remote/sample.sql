634b73d44878b197408e718d613941b09444871a
sunakan <sunakan03@gmail.com>
2025-05-25 19:55:28 +0900

    gtidベースのレプリケーション素振り

---
+++ remote/sample.sql
@@ -0,0 +1,16 @@
+-- reset
+drop database if exists hoge;
+create database hoge;
+use hoge;
+drop table if exists hoge;
+-- ここから
+
+-- datadogで観察
+CREATE TABLE hoge (
+  id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO hoge VALUES (),(),();
+DELETE FROM hoge WHERE id = (SELECT max_id FROM (SELECT MAX(id) AS max_id FROM hoge) AS tmp);
+INSERT INTO hoge VALUES (),(),();
+DELETE FROM hoge WHERE id = (SELECT max_id FROM (SELECT MAX(id) AS max_id FROM hoge) AS tmp);
